9da00887635b02bce4fb8f55b2436fd99c2f70c7
truongthuanhung <hacco1601@gmail.com>
2024-12-25 14:33:12 +0700

    return name, avatar of user comment

--- src/modules/like/dtos/create-like.dto.ts
+++ src/modules/like/dtos/create-like.dto.ts
@@ -1,7 +1,13 @@
 import { IsMongoId } from 'class-validator';
 import { Types } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateLikeDto {
+  @ApiProperty({
+    description: 'The ID of the post to like',
+    type: String,
+    example: '60c72b2f9b1e8a5f5f7e4f8d',
+  })
   @IsMongoId()
   postId: Types.ObjectId;
 }
